df802b6b12b73e6fcf72d9ae4c206a68845d4085
alexanderyee <alexanderyee@email.arizona.edu>
2017-10-12 13:22:26 -0700

    init commit

---
+++ Assets/LandscapeMovement.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LandscapeMovement : MonoBehaviour {
+
+	float mapSpeed = 0.1f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += mapSpeed * Time.deltaTime * Vector3.left;
+	}
+}
